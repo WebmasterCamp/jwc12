92e2a8cf36336c9d9a48781c3f3c31dd712c5805
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-05-23 20:27:14 +0700

    init repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>JWC 12 Landing Page</title>
+        <meta name="description" content="JWC" />
+      </Head>
+
+      <div>JWC 12</div>
+    </div>
+  )
+}
+
+export default Home
