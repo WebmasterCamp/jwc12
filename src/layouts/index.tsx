1a319ef1a9900bacb6b1a77e0ff02c0447dff12f
saenyakorn <saneyakorn@gmail.com>
2022-05-31 23:20:12 +0700

    fix build failed

---
+++ src/layouts/index.tsx
@@ -0,0 +1,21 @@
+import { FunctionComponent, ReactNode } from 'react'
+
+import { Footer } from '@/components/Footer'
+import { ParticlesWidget } from '@/components/Particles'
+
+import { Header } from './document/components/Header'
+
+interface Props {
+  children: ReactNode
+}
+
+export const DocumentLayout: FunctionComponent<Props> = ({ children }) => {
+  return (
+    <div id="document" className="relative min-h-screen px-5 text-black">
+      <ParticlesWidget />
+      <Header />
+      <main className="mx-auto max-w-screen-xl">{children}</main>
+      <Footer />
+    </div>
+  )
+}
--- src/pages/policy/cookies.tsx
+++ src/pages/policy/cookies.tsx
@@ -1,11 +1,9 @@
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 
-import clsx from 'clsx'
-
-import { DocumentLayout } from '../../layouts/document'
-import { ContentWrapper } from '../../layouts/document/components/ContentWrap'
-import { SectionTitle } from '../../layouts/document/components/SectionTitle'
+import { DocumentLayout } from '@/layouts'
+import { SectionTitle } from '@/layouts/document/SectionTitle'
+import { ContentWrapper } from '@/layouts/document/components/ContenWrap'
 
 const Page: NextPage = () => {
   return (
