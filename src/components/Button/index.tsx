3000822fb1cad82fd000f8034a3d9c9fde011fe1
saenyakorn <saneyakorn@gmail.com>
2022-06-04 19:23:55 +0700

    add button color

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,13 +3,15 @@ import { FunctionComponent, forwardRef } from 'react'
 import clsx from 'clsx'
 
 export type ButtonVariants = 'primary' | 'outlined'
+export type ButtonColor = 'primary' | 'gold'
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariants
+  color?: ButtonColor
 }
 
 export const Button: FunctionComponent<ButtonProps> = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant = 'primary', children, ...rest }, ref) => {
+  ({ variant = 'primary', color = 'gold', children, ...rest }, ref) => {
     return (
       <button
         type="button"
@@ -19,8 +21,15 @@ export const Button: FunctionComponent<ButtonProps> = forwardRef<HTMLButtonEleme
           rest.className,
           'rounded-md px-5 py-2 font-medium transition-colors',
           variant === 'outlined' &&
-            'border border-primary bg-white text-primary-accent hover:bg-purple-50',
-          variant === 'primary' && 'bg-primary text-white hover:bg-primary-accent'
+            color === 'primary' &&
+            `border border-primary bg-white text-primary-accent hover:bg-gray-50`,
+          variant === 'primary' &&
+            color === 'primary' &&
+            `bg-primary text-white hover:bg-primary-accent`,
+          variant === 'outlined' &&
+            color === 'gold' &&
+            `border-2 border-gold bg-white text-gold-darker hover:bg-gray-50 hover:border-gold-dark`,
+          variant === 'primary' && color === 'gold' && `bg-gold text-brown-dark hover:bg-gold-dark`
         )}
       >
         {children}
