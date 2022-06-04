b38d2a9bcc24414505978a1df49b0e5a4809d08f
pontakornth <pontakorn61@gmail.com>
2022-06-04 11:47:51 +0700

    feat: Add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from 'react'
+
+import Link from 'next/link'
+
+import clsx from 'clsx'
+
+import { Button } from './Button'
+
+interface Props {
+  className?: string
+}
+
+export const Navbar: FunctionComponent<Props> = ({ className = '' }) => {
+  return (
+    <nav className={clsx('flex px-4 py-2 justify-between items-center', className)}>
+      <Link href={'/'}>
+        <a>
+          <picture>
+            <source srcSet="/images/jwc12-logo.avif" type="image/avif" />
+            <source srcSet="/images/jwc12-logo.webp" type="image/webp" />
+            <img src="/images/jwc12-logo.png" alt="Junior Webmaster Camp 12" className="h-14" />
+          </picture>
+        </a>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <a href="#about">JWC คืออะไร</a>
+        <a href="#schedule">กำหนดการ</a>
+        <a href="#faq">คำถามที่พบบ่อย</a>
+        <a href="#contact">ติดต่อเรา</a>
+        <Button variant="outlined">สมัครค่าย JWC</Button>
+      </div>
+    </nav>
+  )
+}
