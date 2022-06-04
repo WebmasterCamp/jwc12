5960dfc1e2366dccfe313b1961b587485446cd1f
pontakornth <pontakorn61@gmail.com>
2022-06-04 15:41:03 +0700

    Add about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/Button'
 import { Footer } from '@/components/Footer'
 import { Navbar } from '@/components/Navbar'
 import { ParticlesWidget } from '@/components/Particles'
+import { Section } from '@/components/Section'
 
 const Home: NextPage = () => {
   return (
@@ -22,7 +23,26 @@ const Home: NextPage = () => {
           <Button variant="outlined-gold">รู้จักกับค่าย</Button>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 mx-auto"></div>
+      <Section className="flex flex-col-reverse lg:flex-row">
+        <div className="flex-1">
+          <h2 id="about" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
+            รู้จักกับค่ายแห่งโชคชะตา
+          </h2>
+          <p className="">
+            <span className="text-gold">JWC คือ ค่ายสร้างเว็บของเด็กม.ปลายที่อยากกำหนดโชคชะตา</span>{' '}
+            มาร่วมสร้างคำทำนายของตน บนไพ่แห่งโชคชะตาทั้ง 4 สาขาแห่งวงการดิจิทัล ได้แก่ Content,
+            Design, Marketing และ Programming แลกเปลี่ยนความคิดกับผู้กำหนดโชคชะตาด้วยกัน
+            สร้างสรรค์คำทำนายบนไพ่ พร้อมเปิดประสบการณ์ใหม่ในการทำเวิร์คช็อปสุดเข้มข้น
+            แล้วมาท้าชนโชคชะตาล่าอนาคตด้วยกันกับค่าย JWC12 : Make Your Site, Write Your Future
+          </p>
+        </div>
+        <div className="flex-1">Insert video here</div>
+      </Section>
+      <Section className="text-center">
+        <h2 id="schedule" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
+          กำหนดการ
+        </h2>
+      </Section>
       <Footer />
     </div>
   )
