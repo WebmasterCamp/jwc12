14e242141d0da8ea73d43050a1600b7fd0aeefe0
pontakornth <pontakorn61@gmail.com>
2022-06-04 22:08:47 +0700

    feat: Make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 
 import Link from 'next/link'
 
+import BarsIcon from '@iconify/icons-fa6-solid/bars'
+import { Icon } from '@iconify/react'
 import clsx from 'clsx'
 
 import { Button } from './Button'
@@ -11,24 +13,55 @@ interface Props {
 }
 
 export const Navbar: FunctionComponent<Props> = ({ className = '' }) => {
+  const [shown, setShown] = useState(false)
+  const toggleNavbar = () => setShown((shown) => !shown)
   return (
-    <nav className={clsx('flex px-4 py-2 justify-between items-center sticky top-0', className)}>
-      <Link href={'/'}>
-        <a>
-          <picture>
-            <source srcSet="/images/jwc12-logo.avif" type="image/avif" />
-            <source srcSet="/images/jwc12-logo.webp" type="image/webp" />
-            <img src="/images/jwc12-logo.png" alt="Junior Webmaster Camp 12" className="h-14" />
-          </picture>
+    <div className="sticky top-0 z-50 bg-primary">
+      <nav className={clsx('flex px-4 py-2 justify-between items-center', className)}>
+        <Link href={'/'}>
+          <a>
+            <picture>
+              <source srcSet="/images/jwc12-logo.avif" type="image/avif" />
+              <source srcSet="/images/jwc12-logo.webp" type="image/webp" />
+              <img src="/images/jwc12-logo.png" alt="Junior Webmaster Camp 12" className="h-14" />
+            </picture>
+          </a>
+        </Link>
+        <div className="lg:hidden">
+          <Button onClick={toggleNavbar} variant="outlined" className="lg:hidden">
+            <Icon icon={BarsIcon} className="inline-block" /> เมนู
+          </Button>
+        </div>
+        <div className="hidden lg:flex flex-row gap-4 items-center">
+          <a href="#about">JWC คืออะไร</a>
+          <a href="#schedule">กำหนดการ</a>
+          <a href="#faq">คำถามที่พบบ่อย</a>
+          <a href="#contact">ติดต่อเรา</a>
+          <Button variant="gold">สมัครค่าย JWC</Button>
+        </div>
+      </nav>
+      <div
+        className={clsx(
+          'lg:hidden flex-col text-right text-white bg-primary',
+          shown ? 'flex' : 'hidden'
+        )}
+      >
+        <a className="p-4" href="#about">
+          JWC คืออะไร
         </a>
-      </Link>
-      <div className="flex gap-4 items-center">
-        <a href="#about">JWC คืออะไร</a>
-        <a href="#schedule">กำหนดการ</a>
-        <a href="#faq">คำถามที่พบบ่อย</a>
-        <a href="#contact">ติดต่อเรา</a>
-        <Button variant="gold">สมัครค่าย JWC</Button>
+        <a className="p-4" href="#schedule">
+          กำหนดการ
+        </a>
+        <a className="p-4" href="#faq">
+          คำถามที่พบบ่อย
+        </a>
+        <a className="p-4" href="#contact">
+          ติดต่อเรา
+        </a>
+        <div className="p-4">
+          <Button variant="gold">สมัครค่าย JWC</Button>
+        </div>
       </div>
-    </nav>
+    </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,109 +16,113 @@ import { Section } from '@/components/Section'
 
 const Home: NextPage = () => {
   return (
-    <div className="relative flex min-h-screen w-full flex-col px-5 text-white">
-      <Navbar />
-      <ParticlesWidget />
-      <div className="w-full lg:w-1/2 mx-auto">
-        <h1 className="text-4xl lg:text-5xl text-center mt-16 space-y-4 font-heading">
-          Make Your site,
-          <br /> Write Your <span className="text-ct">Future</span>
-        </h1>
-        {/* TODO: Insert sponsor logo here */}
-        <div className=" space-x-4 p-8 text-center mx-auto">TODO: Insert logo</div>
-        <div className="flex flex-col gap-4 lg:gap-8 lg:flex-row lg:justify-center">
-          <Button variant="gold">สมัครค่าย</Button>
-          <Button variant="outlined-gold">รู้จักกับค่าย</Button>
+    <>
+      <Navbar className="text-white" />
+      <div className="relative flex min-h-screen w-full flex-col px-5 text-white">
+        <ParticlesWidget />
+        <div className="w-full lg:w-1/2 mx-auto">
+          <h1 className="text-4xl lg:text-5xl text-center mt-16 space-y-4 font-heading">
+            Make Your site,
+            <br /> Write Your <span className="text-ct">Future</span>
+          </h1>
+          {/* TODO: Insert sponsor logo here */}
+          <div className=" space-x-4 p-8 text-center mx-auto">TODO: Insert logo</div>
+          <div className="flex flex-col gap-4 lg:gap-8 lg:flex-row lg:justify-center">
+            <Button variant="gold">สมัครค่าย</Button>
+            <Button variant="outlined-gold">รู้จักกับค่าย</Button>
+          </div>
         </div>
-      </div>
-      <Section className="flex flex-col-reverse lg:flex-row">
-        <div className="flex-1">
-          <h2 id="about" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
-            รู้จักกับค่ายแห่งโชคชะตา
+        <Section className="flex flex-col-reverse lg:flex-row">
+          <div className="flex-1">
+            <h2 id="about" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
+              รู้จักกับค่ายแห่งโชคชะตา
+            </h2>
+            <p className="">
+              <span className="text-gold font-bold">
+                JWC คือ ค่ายสร้างเว็บของเด็กม.ปลายผู้มีความฝัน อยากฟันฝ่าโชคชะตาและกำหนดอนาคต
+              </span>
+              <br />
+              มาสร้างคำทำนายของตนบนไพ่ทั้ง 4 สาขา แห่งวงการดิจิทัล ได้แก่ Content, Design, Marketing
+              และ Programming แลกเปลี่ยนความคิดกับผู้กำหนดโชคชะตาด้วยกัน ร่วมสร้างสรรค์คำทำนายบนไพ่
+              พร้อมเปิดประสบการณ์ใหม่ในการทำเวิร์คช็อปสุดเข้มข้น แล้วมาท้าชนโชคชะตาล่าอนาคตด้วยกัน
+              กับค่าย JWC12: Make Your Site, Write Your Future
+            </p>
+          </div>
+          <div className="flex-1">Insert video here</div>
+        </Section>
+        <Section className="text-center">
+          <h2 id="schedule" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
+            กำหนดการ
           </h2>
-          <p className="">
-            <span className="text-gold font-bold">
-              JWC คือ ค่ายสร้างเว็บของเด็กม.ปลายผู้มีความฝัน อยากฟันฝ่าโชคชะตาและกำหนดอนาคต
-            </span>
-            <br />
-            มาสร้างคำทำนายของตนบนไพ่ทั้ง 4 สาขา แห่งวงการดิจิทัล ได้แก่ Content, Design, Marketing
-            และ Programming แลกเปลี่ยนความคิดกับผู้กำหนดโชคชะตาด้วยกัน ร่วมสร้างสรรค์คำทำนายบนไพ่
-            พร้อมเปิดประสบการณ์ใหม่ในการทำเวิร์คช็อปสุดเข้มข้น แล้วมาท้าชนโชคชะตาล่าอนาคตด้วยกัน
-            กับค่าย JWC12: Make Your Site, Write Your Future
-          </p>
-        </div>
-        <div className="flex-1">Insert video here</div>
-      </Section>
-      <Section className="text-center">
-        <h2 id="schedule" className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
-          กำหนดการ
-        </h2>
-        <p>TODO: Insert schedule</p>
-      </Section>
-      <Section className="rounded-xl lg:border grid grid-cols-1 lg:grid-cols-2 border-gold lg:w-10/12">
-        <div className="hidden lg:block">
-          <Map />
-        </div>
-        <div>
-          <h2 className="text-3xl font-heading mb-4 font-semibold">สถานที่</h2>
-          <h3 className="text-gold font-heading font-semibold">
-            คณะเทคโนโลยีสารสนเทศ <br></br> สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
-          </h3>
-          <p className="mb-4">เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพฯ</p>
-          <h3 className="text-3xl font-heading mb-4 font-semibold">วิธีการเดินทาง</h3>
-        </div>
-      </Section>
-      <Section className="text-center">
-        <h2 className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
-          เลือกสาขาที่ต้องการสมัคร
-        </h2>
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 text-brown-dark font-heading font-bold text-3xl">
-          {/* TODO: Use actual cards */}
-          <div className="bg-white p-8">Card</div>
-          <div className="bg-white p-8">Card</div>
-          <div className="bg-white p-8">Card</div>
-          <div className="bg-white p-8">Card</div>
-        </div>
-      </Section>
-      <FaqSection />
-      <Section className="text-center">
-        <h2 className="text-3xl lg:text-4l font-semibold font-heading mb-4">ภาพกิจกรรม</h2>
-        <p>TODO: ใส่ภาพกิจกรรม</p>
-      </Section>
-      <Section className="text-center">
-        <h2 className="text-3xl lg:text-4l font-semibold font-heading mb-4">ติดตามข่าวสารของเรา</h2>
-        <div className="space-x-8">
-          <a
-            href="https://www.facebook.com/jwcth"
-            target="_blank"
-            className="hover:text-gold"
-            rel="noreferrer"
-          >
-            <Icon icon={FacebookIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
-          </a>
-          <a
-            href="https://twitter.com/jwcth"
-            className="hover:text-gold"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icon icon={TwitterIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
-          </a>
-          <a
-            href="https://www.instagram.com/jwcth/"
-            target="_blank"
-            className="hover:text-gold"
-            rel="noreferrer"
-          >
-            <Icon icon={InstagramIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
-          </a>
-        </div>
-      </Section>
-      <Section className="text-center">
-        <h2 className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">ผู้สนับสนุน</h2>
-      </Section>
-      <Footer />
-    </div>
+          <p>TODO: Insert schedule</p>
+        </Section>
+        <Section className="rounded-xl lg:border grid grid-cols-1 lg:grid-cols-2 border-gold lg:w-10/12">
+          <div className="hidden lg:block">
+            <Map />
+          </div>
+          <div>
+            <h2 className="text-3xl font-heading mb-4 font-semibold">สถานที่</h2>
+            <h3 className="text-gold font-heading font-semibold">
+              คณะเทคโนโลยีสารสนเทศ <br></br> สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
+            </h3>
+            <p className="mb-4">เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพฯ</p>
+            <h3 className="text-3xl font-heading mb-4 font-semibold">วิธีการเดินทาง</h3>
+          </div>
+        </Section>
+        <Section className="text-center">
+          <h2 className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">
+            เลือกสาขาที่ต้องการสมัคร
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 text-brown-dark font-heading font-bold text-3xl">
+            {/* TODO: Use actual cards */}
+            <div className="bg-white p-8">Card</div>
+            <div className="bg-white p-8">Card</div>
+            <div className="bg-white p-8">Card</div>
+            <div className="bg-white p-8">Card</div>
+          </div>
+        </Section>
+        <FaqSection />
+        <Section className="text-center">
+          <h2 className="text-3xl lg:text-4l font-semibold font-heading mb-4">ภาพกิจกรรม</h2>
+          <p>TODO: ใส่ภาพกิจกรรม</p>
+        </Section>
+        <Section className="text-center">
+          <h2 className="text-3xl lg:text-4l font-semibold font-heading mb-4">
+            ติดตามข่าวสารของเรา
+          </h2>
+          <div className="space-x-8">
+            <a
+              href="https://www.facebook.com/jwcth"
+              target="_blank"
+              className="hover:text-gold"
+              rel="noreferrer"
+            >
+              <Icon icon={FacebookIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
+            </a>
+            <a
+              href="https://twitter.com/jwcth"
+              className="hover:text-gold"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon={TwitterIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
+            </a>
+            <a
+              href="https://www.instagram.com/jwcth/"
+              target="_blank"
+              className="hover:text-gold"
+              rel="noreferrer"
+            >
+              <Icon icon={InstagramIcon} className="mr-3 inline text-3xl sm:text-5xl" inline />
+            </a>
+          </div>
+        </Section>
+        <Section className="text-center">
+          <h2 className="text-3xl font-heading lg:text-4xl mb-4 font-semibold">ผู้สนับสนุน</h2>
+        </Section>
+        <Footer />
+      </div>
+    </>
   )
 }
 
