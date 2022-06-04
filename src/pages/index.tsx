60079023fa286d9bc2e4e62eb13d62aa0fb3a2c0
pontakornth <pontakorn61@gmail.com>
2022-06-04 12:31:40 +0700

    Feat: Add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 
+import { Button } from '@/components/Button'
 import { Footer } from '@/components/Footer'
 import { Navbar } from '@/components/Navbar'
 import { ParticlesWidget } from '@/components/Particles'
@@ -8,6 +9,18 @@ const Home: NextPage = () => {
   return (
     <div className="relative flex min-h-screen w-full flex-col px-5 text-white">
       <Navbar />
+      <div className="w-full lg:w-1/2 mx-auto">
+        <h1 className="text-4xl lg:text-5xl text-center mt-16 space-y-4 font-heading">
+          Make Your site,
+          <br /> Write Your <span className="text-ct">Future</span>
+        </h1>
+        {/* TODO: Insert sponsor logo here */}
+        <div className=" space-x-4 p-8 text-center mx-auto">TODO: Insert logo</div>
+        <div className="flex flex-col gap-4 lg:gap-8 lg:flex-row lg:justify-center">
+          <Button variant="gold">สมัครค่าย</Button>
+          <Button variant="outlined-gold">รู้จักกับค่าย</Button>
+        </div>
+      </div>
       <ParticlesWidget />
       <Footer />
     </div>
