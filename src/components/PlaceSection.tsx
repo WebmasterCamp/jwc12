c0cfaecc67d6faf0d1d2031b1f2dd3c92ae3bf8e
pontakornth <pontakorn61@gmail.com>
2022-06-05 20:36:44 +0700

    feat: Add traveling info

--- src/components/PlaceSection.tsx
+++ src/components/PlaceSection.tsx
@@ -1,6 +1,35 @@
 import { GoogleMap } from './Map'
 import { Section } from './Section'
 
+const travelingInfo = [
+  {
+    method: 'รถไฟสายตะวันออก/ชานเมือง',
+    description: 'ลงที่ป้ายหยุดรถไฟพระจอมเกล้าหรือสถานีหัวตะเข้',
+  },
+  {
+    method: 'SA City Line',
+    description: ' ลงที่สถานีลาดกระบัง แล้วต่อรถไฟที่สถานีรถไฟลาดกระบัง',
+  },
+  {
+    method: 'รถตู้',
+    description: [
+      'หมอชิตใหม่-อนุสาวรีย์–พระจอมเกล้าลาดกระบัง',
+      'รังสิต-ลาดกระบัง',
+      'สาย 50B แฮปปิ้แลนด์-ลาดกระบัง',
+      'สาย 50 ห้างซีคอนสแควร์-พระจอมเกล้าเจ้าคุณทหารลาดกระบัง',
+      'สายมีนบุรี–ลาดกระบัง',
+    ],
+  },
+  {
+    method: 'รถโดยสาร',
+    description: 'สาย 143 (รถร่วมบริการ), สาย 1013 (รถร่วมบริการ)',
+  },
+  {
+    method: 'รถสองแถว',
+    description: 'ศูนย์การค้าแฮปปี้แลนด์–ตลาดหัวตะเข้',
+  },
+]
+
 export const PlaceSection: React.FunctionComponent = () => {
   return (
     <Section className="rounded-xl lg:border grid grid-cols-1 lg:grid-cols-2 border-gold lg:w-10/12">
@@ -14,6 +43,24 @@ export const PlaceSection: React.FunctionComponent = () => {
         </h3>
         <p className="mb-4">เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพฯ</p>
         <h3 className="text-3xl font-heading mb-4 font-semibold">วิธีการเดินทาง</h3>
+        <div>
+          {travelingInfo.map((info) => {
+            return (
+              <div key={info.method}>
+                <span className="font-heading text-gold font-bold">{`${info.method}: `}</span>
+                {typeof info.description === 'string' ? (
+                  <span>{info.description}</span>
+                ) : (
+                  <ul className="list-disc list-inside">
+                    {info.description.map((des) => (
+                      <li key={des}>{des}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            )
+          })}
+        </div>
       </div>
     </Section>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import { GoogleMap } from '@/components/Map'
 import { MovingCard } from '@/components/MovingCard'
 import { Navbar } from '@/components/Navbar'
 import { ParticlesWidget } from '@/components/Particles'
+import { PlaceSection } from '@/components/PlaceSection'
 import { Question } from '@/components/Question'
 import { ScheduleSection } from '@/components/ScheduleSection'
 import { Section } from '@/components/Section'
@@ -60,19 +61,7 @@ const Home: NextPage = () => {
           <div className="flex-1">Insert video here</div>
         </Section>
         <ScheduleSection />
-        <Section className="rounded-xl lg:border grid grid-cols-1 lg:grid-cols-2 border-gold lg:w-10/12">
-          <div className="hidden lg:block">
-            <GoogleMap />
-          </div>
-          <div>
-            <h2 className="text-3xl font-heading mb-4 font-semibold">สถานที่</h2>
-            <h3 className="text-gold font-heading font-semibold">
-              คณะเทคโนโลยีสารสนเทศ <br></br> สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
-            </h3>
-            <p className="mb-4">เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพฯ</p>
-            <h3 className="text-3xl font-heading mb-4 font-semibold">วิธีการเดินทาง</h3>
-          </div>
-        </Section>
+        <PlaceSection />
         <BranchesSection />
         <FaqSection />
         <Section className="text-center">
