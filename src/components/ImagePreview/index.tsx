1ff8d2c11667e20a19648531bb06ce89aeb0c821
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-06 22:31:55 +0700

    Make profile picture square

--- src/components/ImagePreview/index.tsx
+++ src/components/ImagePreview/index.tsx
@@ -19,7 +19,7 @@ export function ImagePreview({ value, className }: ImagePreviewProps) {
           <LoadingAnimation className="w-[148px] h-[148px]" />
         </div>
       ) : (
-        <img className="h-48 rounded-md object-contain" src={imageUrl} alt="" />
+        <img className="w-48 h-48 rounded-md object-cover" src={imageUrl} alt="" />
       )}
     </div>
   )
