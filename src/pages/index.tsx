68cf91395ab9dc6fdeb435f0020ecec3cc30a86a
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-06 21:48:36 +0700

    Remove debug auth component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import { useRef } from 'react'
-
 import type { NextPage } from 'next'
-import dynamic from 'next/dynamic'
 
 import { FollowUsBar } from '@/components/FollowUsBar'
 import { Footer } from '@/components/Footer'
 import { ParticlesWidget } from '@/components/Particles'
-
-const DebugAuth = dynamic(() => import('@/auth/DebugAuth'), { ssr: false })
 
 const Home: NextPage = () => {
   return (
@@ -16,7 +11,6 @@ const Home: NextPage = () => {
       <div className="flex justify-end">
         <FollowUsBar className="m-3 mt-5 sm:my-5 sm:mx-10" />
       </div>
-      <DebugAuth />
 
       <div className="my-10 mx-auto flex min-h-max w-full max-w-screen-md grow flex-col items-center justify-center">
         <div className="relative mx-auto mb-8 w-full max-w-xl xl:max-w-screen-2xl">
