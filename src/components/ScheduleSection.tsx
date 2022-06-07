4a62dd7ce7511942d13a755ffea7d8df3a1c78b5
pontakornth <pontakorn61@gmail.com>
2022-06-07 22:17:28 +0700

    chore: Cleanup unused code

--- src/components/ScheduleSection.tsx
+++ src/components/ScheduleSection.tsx
@@ -5,57 +5,6 @@ import clsx from 'clsx'
 import styles from './ScheduleSection.module.css'
 import { Section } from './Section'
 
-// Code from https://thewebdev.info/2021/09/12/how-to-draw-a-line-between-two-divs-with-javascript/
-const getOffset = (el: HTMLElement) => {
-  const rect = el.getBoundingClientRect()
-  return {
-    left: rect.left + window.pageXOffset,
-    top: rect.top + window.pageYOffset,
-    width: rect.width || el.offsetWidth,
-    height: rect.height || el.offsetHeight,
-  }
-}
-
-const px = (n: number) => `${n}px`
-
-interface ConnectProps {
-  div1: HTMLElement | null
-  div2: HTMLElement | null
-  color: string
-  thickness: number
-}
-
-const Connect: React.FunctionComponent<ConnectProps> = ({ div1, div2, color, thickness }) => {
-  if (div1 === null || div2 === null) return <></>
-  const off1 = getOffset(div1)
-  const off2 = getOffset(div2)
-
-  const x1 = off1.left + off1.width
-  const y1 = off1.top + off1.height
-
-  const x2 = off2.left + off2.width
-  const y2 = off2.top
-
-  const length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-
-  const cx = (x1 + x2) / 2 - length / 2
-  const cy = (y1 + y2) / 2 - thickness / 2
-
-  const angle = Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI)
-
-  const styles = {
-    padding: 0,
-    margin: 0,
-    position: 'absolute',
-    height: px(thickness),
-    left: px(cx),
-    top: px(cy),
-    width: px(length),
-    transform: `rotate(${angle})`,
-  } as CSSProperties
-  return <div style={styles}></div>
-}
-
 const schedules = [
   {
     title: 'รับสมัคร',
@@ -122,9 +71,6 @@ export const ScheduleSection: React.FunctionComponent = () => {
           )
         })}
       </div>
-      {/* {schedulesState.length == 2 && (
-        <Connect div1={schedulesState[0]} div2={schedulesState[1]} thickness={2} color="white" />
-      )} */}
     </Section>
   )
 }
