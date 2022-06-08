f3104e59b281ee7636f5b0e9ed2fd42947d21788
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-08 20:25:51 +0700

    Make branch dialog scrollable

--- src/components/BranchDialog.tsx
+++ src/components/BranchDialog.tsx
@@ -32,7 +32,7 @@ export const BranchDialog: React.FunctionComponent<Props> = ({
       <div className="fixed inset-0 bg-black/30 z-[200]" aria-hidden="true">
         <div className="fixed inset-0 flex items-center justify-center p-8">
           <Dialog.Panel className="flex flex-col justify-between w-full lg:w-1/2">
-            <div className="bg-white space-y-8 p-8 mx-auto rounded">
+            <div className="bg-white space-y-8 p-8 mx-auto rounded-2xl max-h-[calc(100vh_-_72px)] overflow-y-scroll">
               <div className="flex justify-between items-center mb-8">
                 <Dialog.Title className="font-bold text-lg lg:text-3xl text-brown-dark font-heading">
                   {title}
