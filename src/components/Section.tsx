13c57a6489b99f9ab1c44897f7882741a78bf062
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-08 19:58:56 +0700

    Make branch cards scrollable on mobile

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -4,9 +4,18 @@ import clsx from 'clsx'
 
 interface Props {
   className?: string
+  noPadding?: boolean
   children?: React.ReactNode
 }
 
-export const Section: FunctionComponent<Props> = ({ className = '', children }) => {
-  return <div className={clsx('w-full p-4 lg:p-32 mx-auto my-16', className)}>{children}</div>
+export const Section: FunctionComponent<Props> = ({
+  className = '',
+  noPadding = false,
+  children,
+}) => {
+  return (
+    <div className={clsx('w-full lg:p-32 mx-auto my-16', !noPadding && 'p-4', className)}>
+      {children}
+    </div>
+  )
 }
