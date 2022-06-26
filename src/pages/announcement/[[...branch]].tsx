c070d62a564d6553e93b18bef6187db9b16dca1c
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-26 16:32:04 +0700

    Fix announcement export

--- src/pages/announcement/[[...branch]].tsx
+++ src/pages/announcement/[[...branch]].tsx
@@ -1,1 +1,23 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+
 export { Announcement as default } from '@/modules/announcement'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const branches = ['programming', 'design', 'marketing', 'content']
+  const branchPaths = branches.map((branch) => ({
+    params: {
+      branch: [branch],
+    },
+  }))
+  const paths = [{ params: { branch: [] } }, ...branchPaths]
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<{}> = async () => {
+  return {
+    props: {},
+  }
+}
