fa5d58949f292381a17f9837e9cb7517f2fadcfd
saenyakorn <saneyakorn@gmail.com>
2022-06-28 01:48:18 +0700

    fix sposors responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ import { BranchesSection } from '@/components/BranchesSection'
 import { LinkButton } from '@/components/Button'
 import { FaqSection } from '@/components/FaqSection'
 import { Footer } from '@/components/Footer'
-import { Logo } from '@/components/Logo'
 import { MovingCards } from '@/components/MovingCards'
 import { Navbar } from '@/components/Navbar'
 import { ParticlesWidget } from '@/components/Particles'
@@ -36,7 +35,7 @@ const Home: NextPage = () => {
       <Navbar className="text-white" />
       <div className="flex min-h-screen overflow-x-hidden w-full flex-col px-5 text-white">
         <ParticlesWidget />
-        <div className="w-full lg:w-1/2 mx-auto pt-16">
+        <div className="w-full lg:w-3/4 mx-auto pt-16">
           <div className="flex gap-x-4 items-center justify-center">
             <Star />
             <p className="text-gold-dark whitespace-nowrap text-center">
@@ -147,83 +146,42 @@ const Home: NextPage = () => {
 }
 
 const withLink = (url: string, children: ReactNode) => (
-  <a href={url} target="_blank" rel="noreferrer">
+  <a
+    href={url}
+    target="_blank"
+    rel="noreferrer"
+    className="flex flex-row justify-center items-center"
+  >
     {children}
   </a>
 )
 
 function SponsorLogosOnHero() {
   return (
-    <div className="space-x-4 text-center mx-auto flex flex-row items-center justify-center gap-x-6 gap-y-10 flex-wrap my-20">
-      {withLink(
-        'https://www.webmaster.or.th/',
-        <img
-          className="h-[48px] cursor-pointer"
-          src="/images/sponsors/01_TWA.svg"
-          alt="Thai Webmaster Association"
-        />
-      )}
-      {withLink(
-        'https://www.it.kmitl.ac.th/',
-        <img className="h-[48px] cursor-pointer" src="/images/sponsors/02_IT.svg" alt="IT KMITL" />
-      )}
-      {withLink(
-        'https://www.cpall.co.th/',
-        <img
-          className="h-[48px] cursor-pointer"
-          src="/images/sponsors/VIP_01_CP.svg"
-          alt="CP All"
-        />
-      )}
-      {withLink(
-        'https://www.scb.co.th/',
-        <img className="h-[48px] cursor-pointer" src="/images/sponsors/VIP_02_SCB.svg" alt="SCB" />
-      )}
-      {withLink(
-        'https://foretoday.asia/ ',
-        <img
-          className="h-[48px] cursor-pointer"
-          src="/images/sponsors/VIP_03_ForeToday.svg"
-          alt="Fore Today"
-        />
-      )}
-      {withLink(
-        'https://www.shippop.com/',
-        <img
-          className="h-[48px] cursor-pointer"
-          src="/images/sponsors/VIP_04_Shippop.svg"
-          alt="Shippop"
-        />
-      )}
-    </div>
-  )
-}
-
-function SponsorLogosOnBottom() {
-  return (
-    <div className="space-x-4 p-8 text-center mx-auto flex flex-col items-center justify-center gap-y-[48px]">
-      {/** VIP */}
-      <div className="flex flex-wrap flex-1 gap-x-[56px] gap-y-[72px] justify-center items-center">
+    <div className="space-x-4 text-center mx-auto flex flex-row items-center justify-center gap-x-6 flex-wrap my-20">
+      <div className="flex flex-row flex-wrap basis-full justify-center gap-x-[56px] gap-y-[56px] mb-[20px]">
         {withLink(
           'https://www.webmaster.or.th/',
           <img
-            className="h-[60px] cursor-pointer"
-            src="/images/sponsors/01_TWA.svg"
+            className="h-[36px] md:h-[72px] cursor-pointer"
+            src="/images/sponsors/VIP0A_TWA.svg"
             alt="Thai Webmaster Association"
           />
         )}
         {withLink(
           'https://www.it.kmitl.ac.th/',
           <img
-            className="h-[60px] cursor-pointer"
-            src="/images/sponsors/02_IT.svg"
+            className="h-[44px] md:h-[88px] cursor-pointer"
+            src="/images/sponsors/VIP0B_IT.svg"
             alt="IT KMITL"
           />
         )}
+      </div>
+      <div className="flex flex-row flex-wrap basis-full justify-center items-end gap-x-[56px] gap-y-[56px] mb-[70px]">
         {withLink(
           'https://www.cpall.co.th/',
           <img
-            className="h-[60px] cursor-pointer"
+            className="h-[48px] md:h-[96px] cursor-pointer"
             src="/images/sponsors/VIP_01_CP.svg"
             alt="CP All"
           />
@@ -231,15 +189,17 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.scb.co.th/',
           <img
-            className="h-[60px] cursor-pointer"
+            className="h-[28px] md:h-[56px] cursor-pointer"
             src="/images/sponsors/VIP_02_SCB.svg"
             alt="SCB"
           />
         )}
+      </div>
+      <div className="flex flex-row flex-wrap basis-full justify-center items-end gap-x-[56px] gap-y-[56px]">
         {withLink(
           'https://foretoday.asia/ ',
           <img
-            className="h-[60px] cursor-pointer"
+            className="h-[30px] md:h-[60px] cursor-pointer"
             src="/images/sponsors/VIP_03_ForeToday.svg"
             alt="Fore Today"
           />
@@ -247,7 +207,65 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.shippop.com/',
           <img
-            className="h-[60px] cursor-pointer"
+            className="h-[24px] md:h-[48px] cursor-pointer"
+            src="/images/sponsors/VIP_04_Shippop.svg"
+            alt="Shippop"
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+function SponsorLogosOnBottom() {
+  return (
+    <div className="space-x-4 text-center mx-auto flex flex-col items-center justify-center gap-y-[112px]">
+      {/** VIP */}
+      <div className="flex flex-wrap flex-1 gap-x-[56px] gap-y-[72px] justify-center items-center">
+        {withLink(
+          'https://www.webmaster.or.th/',
+          <img
+            className="h-[48px] md:h-[96px] cursor-pointer"
+            src="/images/sponsors/VIP0A_TWA.svg"
+            alt="Thai Webmaster Association"
+          />
+        )}
+        {withLink(
+          'https://www.it.kmitl.ac.th/',
+          <img
+            className="h-[52px] md:h-[104px] cursor-pointer"
+            src="/images/sponsors/VIP0B_IT.svg"
+            alt="IT KMITL"
+          />
+        )}
+        {withLink(
+          'https://www.cpall.co.th/',
+          <img
+            className="h-[62px] md:h-[124px] cursor-pointer"
+            src="/images/sponsors/VIP_01_CP.svg"
+            alt="CP All"
+          />
+        )}
+        {withLink(
+          'https://www.scb.co.th/',
+          <img
+            className="h-[36px] md:h-[72px] cursor-pointer"
+            src="/images/sponsors/VIP_02_SCB.svg"
+            alt="SCB"
+          />
+        )}
+        {withLink(
+          'https://foretoday.asia/ ',
+          <img
+            className="h-[39px] md:h-[78px] cursor-pointer"
+            src="/images/sponsors/VIP_03_ForeToday.svg"
+            alt="Fore Today"
+          />
+        )}
+        {withLink(
+          'https://www.shippop.com/',
+          <img
+            className="h-[35px] md:h-[70px] cursor-pointer"
             src="/images/sponsors/VIP_04_Shippop.svg"
             alt="Shippop"
           />
@@ -259,7 +277,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.brikl.com/',
           <img
-            className="h-[48px] cursor-pointer"
+            className="h-[36px] md:h-[72px] cursor-pointer"
             src="/images/sponsors/SP_01_Brikl.svg"
             alt="Brikl"
           />
@@ -267,15 +285,15 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://cleverse.com/ ',
           <img
-            className="h-[48px] cursor-pointer"
-            src="/images/sponsors/P_02_Camphub.svg"
-            alt="Camphub"
+            className="h-[44px] md:h-[88px] cursor-pointer"
+            src="/images/sponsors/SP_02_Cleverse.svg"
+            alt="Cleverse"
           />
         )}
         {withLink(
           'https://www.microsoft.com/th-th',
           <img
-            className="h-[48px] cursor-pointer"
+            className="h-[36px] md:h-[72px] cursor-pointer"
             src="/images/sponsors/SP_03_Microsoft.svg"
             alt="Microsoft"
           />
@@ -283,14 +301,18 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.viabus.co/',
           <img
-            className="h-[48px] cursor-pointer"
+            className="h-[36px] md:h-[72px] cursor-pointer"
             src="/images/sponsors/SP_04_VIABUS.svg"
             alt="ViaBus"
           />
         )}
         {withLink(
           '/',
-          <img className="h-[48px] cursor-pointer" src="/images/sponsors/SP_05_NUT.svg" alt="NUT" />
+          <img
+            className="h-[36px] md:h-[72px] cursor-pointer"
+            src="/images/sponsors/SP_05_NUT.svg"
+            alt="NUT"
+          />
         )}
       </div>
 
@@ -299,7 +321,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.facebook.com/imaginethailandmovement',
           <img
-            className="h-[36px] cursor-pointer"
+            className="h-[40px] md:h-[80px] cursor-pointer"
             src="/images/sponsors/P_01_ImgTH.svg"
             alt="Imagine"
           />
@@ -307,7 +329,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.camphub.in.th/',
           <img
-            className="h-[36px] cursor-pointer"
+            className="h-[44px] md:h-[88px] cursor-pointer"
             src="/images/sponsors/P_02_Camphub.svg"
             alt="camphub"
           />
@@ -315,7 +337,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://pantip.com/',
           <img
-            className="h-[36px] cursor-pointer"
+            className="h-[40px] md:h-[80px] cursor-pointer"
             src="/images/sponsors/P_03_Pantip.svg"
             alt="Pantip"
           />
@@ -323,7 +345,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           '/',
           <img
-            className="h-[36px] cursor-pointer"
+            className="h-[40px] md:h-[80px] cursor-pointer"
             src="/images/sponsors/P_04_YWC-Luxury.svg"
             alt="YWC Luxury"
           />
@@ -331,7 +353,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://spaceth.co/',
           <img
-            className="h-[36px] cursor-pointer"
+            className="h-[40px] md:h-[80px] cursor-pointer"
             src="/images/sponsors/P_05_Spaceth.svg"
             alt="Spaceth"
           />
@@ -343,7 +365,7 @@ function SponsorLogosOnBottom() {
         {withLink(
           'https://www.dek-d.com/',
           <img
-            className="h-[30px] cursor-pointer"
+            className="h-[40px] md:h-[60px] cursor-pointer"
             src="/images/sponsors/S_01_Dek-d.svg"
             alt="Dek-d"
           />
@@ -355,7 +377,7 @@ function SponsorLogosOnBottom() {
 
 function MediaPartner() {
   return (
-    <div className="space-x-4 p-8 text-center mx-auto flex flex-col items-center justify-center gap-y-[48px]">
+    <div className="space-x-4 text-center mx-auto flex flex-col items-center justify-center gap-y-[48px]">
       <div className="flex flex-wrap flex-1 gap-x-[60px] gap-y-[48px] justify-center items-center">
         {withLink(
           'https://baseplayhouse.co/',
