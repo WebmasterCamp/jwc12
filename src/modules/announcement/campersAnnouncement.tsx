0eabe9153dace6db86e9eda7a98ca7efc56a9a57
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-29 14:08:39 +0700

    Campers announcement page

---
+++ src/modules/announcement/campersAnnouncement.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+
+import { Loading } from '@/components/Loading'
+import { useCampers } from '@/db/hooks'
+import { Camper } from '@/db/types'
+
+import { AnnouncementTemplate } from './template'
+import { AnnouncementColumn } from './types'
+
+const columns: AnnouncementColumn<Camper>[] = [
+  {
+    key: 'id',
+    label: 'รหัส',
+  },
+  {
+    key: 'name',
+    label: 'ชื่อ - นามสกุล',
+  },
+]
+
+export function CampersAnnouncement() {
+  const { pending, data } = useCampers()
+
+  const campers = useMemo(() => data?.data ?? [], [data])
+
+  if (pending) {
+    return <Loading />
+  }
+
+  return (
+    <AnnouncementTemplate
+      title="Campers"
+      tableHeader="รายชื่อ camper"
+      linkPrefix="/campers/"
+      candidates={campers}
+      columns={columns}
+    >
+      <h2 className="text-lg font-bold mb-4">TODO</h2>
+      <p className="mb-6">Update this</p>
+    </AnnouncementTemplate>
+  )
+}
